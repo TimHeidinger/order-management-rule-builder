eba34b7d71f1bc58e258c8a0eea9a136e217b674
simonbrockhaus <spam.simon.b@gmail.com>
2021-01-17 14:56:58 +0100

    created a test drop-down-menu

---
+++ src/app/menu-overview-example/menu-overview-example.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuOverviewExampleComponent } from './menu-overview-example.component';
+
+describe('MenuOverviewExampleComponent', () => {
+  let component: MenuOverviewExampleComponent;
+  let fixture: ComponentFixture<MenuOverviewExampleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuOverviewExampleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuOverviewExampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

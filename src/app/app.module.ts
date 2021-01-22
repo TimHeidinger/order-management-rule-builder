6ee5bc0480f25245ed7bd4cc80eb4f06fe0171a4
simonbrockhaus <spam.simon.b@gmail.com>
2021-01-22 21:21:36 +0100

    button design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { MenuOverviewExampleComponent } from './menu-overview-example/menu-overview-example.component';
 import {MatMenuModule} from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownBasicComponent } from './dropdown-basic/dropdown-basic.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuOverviewExampleComponent
+    MenuOverviewExampleComponent,
+    DropdownBasicComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatMenuModule
+    MatMenuModule,
+    NgbModule
     
   ],
   providers: [],

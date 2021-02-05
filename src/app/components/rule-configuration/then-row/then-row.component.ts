c0e6dc909ac29e1026e7df96de485c8e28f935b3
Mert Uras <mert.uras.13@gmail.com>
2021-02-05 16:35:04 +0100

    thenRow

--- src/app/components/rule-configuration/then-row/then-row.component.ts
+++ src/app/components/rule-configuration/then-row/then-row.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { getLocaleDateFormat, getLocaleTimeFormat } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
+  
   selector: 'app-then-row',
   templateUrl: './then-row.component.html',
   styleUrls: ['./then-row.component.css']
 })
+
 export class ThenRowComponentComponent implements OnInit {
+
+  public construction: string;
+  public constructionNumber: number;
+  public objekt: string;
+  public objektNumber: number;
+  public date: string;
+  public dateNumber: number;
+  public opt: string;
+  public optNumber: number;
+  public offset: string;
+  public nu: number;
+
+  
+  exportlog() {
+
+    console.log(this.constructionNumber);
+    console.log(this.objektNumber);
+    console.log(this.date);
+    console.log(this.opt);
+    console.log(this.offset);
+    
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.construction = 'Set';
+    this.objekt = "Shipment Request";
+    this.date = "Urgent Date";
+    this.opt = "OrderPlacedTime";
+    this.offset = "offset";
+    
   }
 
+  reset(){
+    this.construction = 'Delete';
+    this.objekt = "Shipment Request";
+    this.date = "Urgent Date";
+    this.opt = "OrderPlacedTime";
+    this.offset = "offset";
+    
+  }
 }

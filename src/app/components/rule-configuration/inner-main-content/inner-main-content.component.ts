30e8a2ed737cd700bf21a2df0af19967c908d96d
simonbrockhaus <spam.simon.b@gmail.com>
2021-02-07 20:40:12 +0100

    add button variabler text

--- src/app/components/rule-configuration/inner-main-content/inner-main-content.component.ts
+++ src/app/components/rule-configuration/inner-main-content/inner-main-content.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inner-main-content.component.css']
 })
 export class InnerMainContentComponent implements OnInit {
+  public text1: string = "test1";
+  public text2: string = "test2";
 
   constructor() { }
 

782000373ea1103f2b20bcd98792dda8bcff6929
simonbrockhaus <spam.simon.b@gmail.com>
2021-02-15 19:05:56 +0100

    dummy data

--- src/app/components/rule-configuration/inner-main-content/inner-main-content.component.ts
+++ src/app/components/rule-configuration/inner-main-content/inner-main-content.component.ts
@@ -15,6 +15,16 @@ export class InnerMainContentComponent implements OnInit {
   public text5: number = 1;
   public text6: number = 2;
 
+
+ public ruleName: String ='testname';
+ public ifChoiceDelivery: String ='home';
+ public ifChoiceCarrier: String ='express';
+ public thenChoiceDue: Number = 5;
+ public thenChoiceDueTime: Number = 1500;
+ public thenChoiceUrgent: Number = 2;
+ public thenChoiceUrgentTime: Number = 2000;
+
+
   constructor() { }
 
   ngOnInit(): void {

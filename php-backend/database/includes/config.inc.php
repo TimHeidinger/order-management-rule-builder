b7e9baad0f56b38b2158c3fb3b1ebcc652a03d33
Tim Heidinger <mail@tim-heidinger.com>
2021-02-17 11:02:33 +0100

    update backend config credentials

--- php-backend/database/includes/config.inc.php
+++ php-backend/database/includes/config.inc.php
@@ -1,6 +1,6 @@
 <?php
-$host = "database-2.cezj1xvd0imm.eu-central-1.rds.amazonaws.com";
+$host = "";
 $port = 5432;
-$username = "nemo_system";
-$password = "ApEuyHFR4gmNGQHoDHqpMGTphsTDmdfi3xKq3SN7dR8DD2GobRcXX5bL";
-$table = "oasis_system";
+$username = "";
+$password = "";
+$table = "";

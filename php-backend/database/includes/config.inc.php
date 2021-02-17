a12442fd5c1ecf7b1525d2e8a5544a59fef55128
Tim Heidinger <mail@tim-heidinger.com>
2021-02-17 10:52:09 +0100

    added aws deploy script

---
+++ php-backend/database/includes/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+$host = "database-2.cezj1xvd0imm.eu-central-1.rds.amazonaws.com";
+$port = 5432;
+$username = "nemo_system";
+$password = "ApEuyHFR4gmNGQHoDHqpMGTphsTDmdfi3xKq3SN7dR8DD2GobRcXX5bL";
+$table = "oasis_system";

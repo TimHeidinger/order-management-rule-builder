886236d1b9275a9e4fb9be74d54e44caee57951a
Mert Uras <mert.uras.13@gmail.com>
2021-02-19 16:34:20 +0100

    overview-innermain

--- src/app/components/overview/inner-overview/inner-overview.component.ts
+++ src/app/components/overview/inner-overview/inner-overview.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Rule {
+  position: number;
+  name: string;
+  dat: number;
+}
+const RULES: Rule[]=[
+  {
+    position: 1,
+    name: 'bps1',
+    dat: 1500
+  },{
+    position: 2,
+    name: 'bps2',
+    dat: 1600
+  },
+  {
+    position: 3,
+    name: 'bps3',
+    dat: 999
+  }
+];
+
 @Component({
   selector: 'app-inner-overview',
   templateUrl: './inner-overview.component.html',
   styleUrls: ['./inner-overview.component.css']
 })
-export class InnerOverviewComponent implements OnInit {
 
-  constructor() { }
+export class InnerOverviewComponent implements OnInit {
+ rules=RULES;
+  constructor() {}
 
   ngOnInit(): void {
   }

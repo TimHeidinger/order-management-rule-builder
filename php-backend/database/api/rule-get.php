04b3eb29bf41fdd8b922ce097df64f3ecc97fcfa
Tim Heidinger <mail@tim-heidinger.com>
2021-02-20 23:37:29 +0100

    fetch and insert php api support

---
+++ php-backend/database/api/rule-get.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../includes/connection.inc.php';
+
+$id = $_GET['id'];
+
+$conn = getDatabaseConnection();
+if ($id != NULL) {
+	$query = "SELECT row_to_json (aoe_rules) FROM aoe_rules WHERE id = ?";
+	$stmt = $conn->prepare($query);
+	$stmt->bindParam(1, $id);
+} else {
+	$query = "SELECT row_to_json (aoe_rules) FROM aoe_rules";
+	$stmt = $conn->prepare($query);
+}
+
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+$count = count($result);
+$output = "[";
+for ($i = 0; $i < $count; $i++) {
+	$output = $output . $result[$i][0];
+	if (($i + 1) != $count) {
+		$output = $output . ',';
+	}
+}
+$output = $output . ']';
+echo ($output);

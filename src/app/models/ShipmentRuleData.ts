845ff3580d5b043ec5ad31f519bb3f8691fa1986
simonbrockhaus <spam.simon.b@gmail.com>
2021-02-22 19:00:26 +0100

    fix error browser console

--- src/app/models/ShipmentRuleData.ts
+++ src/app/models/ShipmentRuleData.ts
@@ -8,9 +8,15 @@ export class ShipmentRuleData {
 
     ifChoiceDelivery: String;
     ifChoiceCarrier: String;
+    ifRelation1: String;
+    ifRelation2: String;
+    thenChoiceDue: any; //TODO: Add type
     thenChoiceDueTime: any; //TODO: Add type
     thenChoiceUrgend: any; //TODO: Add type
     thenCoiceUrgendTime: any; //TODO: Add type
+  
+   
+
 
     /**
      * @returns JSON string of this instance
